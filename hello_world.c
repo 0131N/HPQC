ee7db40f16f5ead20b061ab1fa4a2349d73ef9db
Oisín <oisin.corrigan6@mail.dcu.ieecho>
2026-01-28 16:36:31 +0000

    Practice

---
+++ hello_world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Based on Parewa Labs Pvt. Ltd. 
+// Available online at
+// https://www.programiz.com/c-programming/examples/print-sentence
+// 2025-01-09
+
+int main() 
+{
+   // printf() displays the string inside quotation
+   printf("dippy!\n");
+   return 0;
+}
