aa0ea348bee3fe2e3b937eb6b9f42d86d9314525
Oisín <oisin.corrigan6@mail.dcu.ieecho>
2026-02-17 17:38:01 +0000

    Changed master logic

--- week_3/comm_test_mpi.c
+++ week_3/comm_test_mpi.c
@@ -1,22 +1,16 @@
-@@ -0,0 +1,42 @@
-// comm_test_mpi_v3.c
+// comm_test_mpi_v2.c
 #include <stdio.h>
 #include <mpi.h>
 
+// NEW FUNCTION: Handles everything Rank 0 needs to do
 void run_master(int total_processes) {
     int recv_message;
     MPI_Status status;
+
     for (int i = 1; i < total_processes; i++) {
         MPI_Recv(&recv_message, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
         printf("[Master] Received %d from Rank %d\n", recv_message, i);
     }
-}
-
-// NEW FUNCTION: Handles everything other ranks need to do
-void run_worker(int rank) {
-    int send_message = rank * 10;
-    MPI_Send(&send_message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    printf("[Worker] Rank %d sent %d\n", rank, send_message);
 }
 
 int main(int argc, char **argv) {
@@ -28,10 +22,13 @@ int main(int argc, char **argv) {
 
     if (uni_size > 1) {
         if (my_rank == 0) {
-            run_master(uni_size);
+            run_master(uni_size); // CALL THE NEW FUNCTION
         } 
         else {
-            run_worker(my_rank); // CALL THE NEW FUNCTION
+            // Worker logic is still stuck here
+            int send_message = my_rank * 10;
+            MPI_Send(&send_message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            printf("[Worker] Rank %d sent %d\n", my_rank, send_message);
         }
     } 
     else {
