0c3bf8f21c2fbcb3d60c57b3062c63713c351a03
Oisín <oisin.corrigan6@mail.dcu.ieecho>
2026-02-17 17:39:09 +0000

    Combined master and worker logic

--- week_3/comm_test_mpi.c
+++ week_3/comm_test_mpi.c
@@ -1,16 +1,24 @@
-// comm_test_mpi_v2.c
+// comm_test_mpi_final.c
 #include <stdio.h>
 #include <mpi.h>
 
-// NEW FUNCTION: Handles everything Rank 0 needs to do
 void run_master(int total_processes) {
     int recv_message;
     MPI_Status status;
-
+    
+    printf("--- Master Starting Reception ---\n");
     for (int i = 1; i < total_processes; i++) {
         MPI_Recv(&recv_message, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
         printf("[Master] Received %d from Rank %d\n", recv_message, i);
     }
+}
+
+void run_worker(int rank) {
+    int send_message = rank * 10;
+    
+    // We can change the tag here (0) to something meaningful if needed
+    MPI_Send(&send_message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    printf("[Worker %d] Message sent.\n", rank);
 }
 
 int main(int argc, char **argv) {
@@ -20,19 +28,18 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &uni_size);
 
-    if (uni_size > 1) {
-        if (my_rank == 0) {
-            run_master(uni_size); // CALL THE NEW FUNCTION
-        } 
-        else {
-            // Worker logic is still stuck here
-            int send_message = my_rank * 10;
-            MPI_Send(&send_message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-            printf("[Worker] Rank %d sent %d\n", my_rank, send_message);
-        }
-    } 
-    else {
-        printf("Error: Need at least 2 processes.\n");
+    // GUARD CLAUSE: Check for errors first, then proceed
+    if (uni_size < 2) {
+        if (my_rank == 0) printf("Error: Run with at least 2 processes (e.g. -np 2)\n");
+        MPI_Finalize();
+        return 1;
+    }
+
+    // Clean logic flow
+    if (my_rank == 0) {
+        run_master(uni_size);
+    } else {
+        run_worker(my_rank);
     }
 
     MPI_Finalize();
