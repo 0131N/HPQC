a2a5e88182f65de1ee57a668cb3a17d305390af7
Oisín <oisin.corrigan6@mail.dcu.ieecho>
2026-02-17 17:19:23 +0000

    week_3

--- week_3/comm_test_mpi.c
+++ week_3/comm_test_mpi.c
@@ -1,21 +1,23 @@
-// comm_test_mpi_v3.c
+// comm_test_mpi_final.c
 #include <stdio.h>
 #include <mpi.h>
 
 void run_master(int total_processes) {
     int recv_message;
     MPI_Status status;
+    printf("--- Master Starting Reception ---\n");
     for (int i = 1; i < total_processes; i++) {
         MPI_Recv(&recv_message, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
         printf("[Master] Received %d from Rank %d\n", recv_message, i);
     }
 }
 
-// NEW FUNCTION: Handles everything other ranks need to do
 void run_worker(int rank) {
     int send_message = rank * 10;
+
+    // can change the tag here to something meaningful
     MPI_Send(&send_message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    printf("[Worker] Rank %d sent %d\n", rank, send_message);
+    printf("[Worker %d] Message sent.\n", rank);
 }
 
 int main(int argc, char **argv) {
@@ -24,16 +26,18 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &uni_size);
 
-    if (uni_size > 1) {
-        if (my_rank == 0) {
-            run_master(uni_size);
-        } 
-        else {
-            run_worker(my_rank); // CALL THE NEW FUNCTION
-        }
-    } 
-    else {
-        printf("Error: Need at least 2 processes.\n");
+    // GUARD CLAUSE: Check for errors first, then proceed
+    if (uni_size < 2) {
+        if (my_rank == 0) printf("Error: Run with at least 2 processes (e.g. -np 2)\n");
+        MPI_Finalize();
+        return 1;
+    }
+
+    // Clean logic flow
+    if (my_rank == 0) {
+        run_master(uni_size);
+    } else {
+        run_worker(my_rank);
     }
 
     MPI_Finalize();
